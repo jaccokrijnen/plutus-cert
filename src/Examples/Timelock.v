bdf185075155b6b018d6a778b13409bbdf0c48a3
Dekentje <jorisjdral@gmail.com>
2021-08-02 09:53:12 +0200

    Remove obsolete lines instead of commenting them out

--- src/Examples/Timelock.v
+++ src/Examples/Timelock.v
@@ -24,8 +24,7 @@ From PlutusCert Require Import Language.PlutusIR.Transform.LetNonRec.
 From PlutusCert Require Import Language.PlutusIR.Transform.Desugar.DecideBool.
 (* From PlutusCert Require Import Language.PlutusIR.Transform.ScottEnc. *)
 From PlutusCert Require Import Language.PlutusIR.Optimizer.DeadBindings.
-(* EDIT_JORIS : From PlutusCert Require Import Language.PlutusIR.Examples. *)
-(* EDIT_JORIS : *) From PlutusCert Require Import Examples.TimelockDumps.
+From PlutusCert Require Import Examples.TimelockDumps.
 
 Local Open Scope string_scope.
 
