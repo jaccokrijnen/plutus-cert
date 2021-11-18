4be59a666d394b835488b0696c61d550def60379
Dekentje <jorisjdral@gmail.com>
2021-11-18 18:07:59 +0100

    Full proof for substitution_preserves_typing

--- src/Language/PlutusIR/Semantics/TypeSafety/SubstitutionPreservesTyping/TermSubstitution.v
+++ src/Language/PlutusIR/Semantics/TypeSafety/SubstitutionPreservesTyping/TermSubstitution.v
@@ -4,7 +4,234 @@ Require Import PlutusCert.Language.PlutusIR.Semantics.Static.
 Require Import PlutusCert.Language.PlutusIR.Semantics.Dynamic.Substitution.
 
 Require Import Coq.Logic.FunctionalExtensionality.
+Require Import Coq.Lists.List.
 
+
+
+(** * Helper lemmas *)
+
+Lemma subst_b__preserves_bindsD : forall b v x,
+    binds_Delta <{ [v / x][b] b }> = binds_Delta b.
+Proof with eauto.
+  intros.
+  destruct b.
+  all: simpl.
+  - destruct v0...
+  - destruct t...
+  - destruct d...
+Qed.
+
+Lemma subst_b__preserves_bindsG : forall b v x,
+    binds_Gamma <{ [v / x][b] b }> = binds_Gamma b.
+Proof with eauto.
+  intros.
+  destruct b.
+  all: simpl.
+  - destruct v0...
+  - destruct t...
+  - destruct d...
+Qed.
+
+Lemma subst_bnr__preserves_bindsD : forall bs v x,
+    map binds_Delta <{ [v / x][bnr] bs }> = map binds_Delta bs.
+Proof with eauto.
+  induction bs; intros...
+  - simpl.
+    destruct (existsb (eqb x) (bvb a)).
+    all: simpl.
+    all: f_equal.
+    all: eauto using subst_b__preserves_bindsD.
+Qed.
+
+Lemma subst_bnr__preserves_bindsG : forall bs v x,
+    map binds_Gamma <{ [v / x][bnr] bs }> = map binds_Gamma bs.
+Proof with eauto.
+  induction bs; intros...
+  - simpl.
+    destruct (existsb (eqb x) (bvb a)).
+    all: simpl.
+    all: f_equal.
+    all: eauto using subst_b__preserves_bindsG.
+Qed.
+
+Lemma subst_br__preserves_bindsD : forall bs v x,
+    map binds_Delta <{ [v / x][br] bs }> = map binds_Delta bs.
+Proof with eauto.
+  induction bs; intros...
+  - simpl.
+    f_equal.
+    all: eauto using subst_b__preserves_bindsD.
+Qed.
+
+Lemma subst_br__preserves_bindsG : forall bs v x,
+    map binds_Gamma <{ [v / x][br] bs }> = map binds_Gamma bs.
+Proof with eauto.
+  induction bs; intros...
+  - simpl.
+    f_equal.
+    all: eauto using subst_b__preserves_bindsG.
+Qed.
+
+Lemma In_bvc_constrBind : forall cs x d,
+    In x (map bvc cs) ->
+    In x (map fst (map (constrBind d) cs)).
+Proof with eauto.
+  induction cs. all: intros.
+  - simpl in H. simpl...
+  - destruct a as [[y T] ar].
+    destruct d.
+    simpl in H.
+    destruct H.
+    all: simpl...
+Qed.
+
+Lemma notIn_bvc_constrBind : forall cs x d,
+    ~ In x (map bvc cs) ->
+    ~ In x (map fst (map (constrBind d) cs)).
+Proof with eauto.
+  induction cs. all: intros.
+  - simpl in H. simpl...
+  - destruct a as [[y T] ar].
+    destruct d.
+    simpl in H.
+    intros Hcon.
+    simpl in Hcon.
+    destruct Hcon...
+    eapply IHcs...
+Qed.
+
+Lemma In_bvb_bindsG: forall x b,
+    In x (bvb b) ->
+    In x (map fst (binds_Gamma b)).
+Proof with eauto.
+  intros.
+  destruct b.
+  all: simpl in H.
+  - destruct v.
+    simpl in H.
+    simpl...
+  - destruct t.
+    simpl in H.
+    simpl...
+  - destruct d.
+    destruct t.
+    simpl in H.
+    simpl.
+    destruct H.
+    + subst...
+    + right...
+      apply in_rev in H.
+      rewrite map_rev.
+      apply -> in_rev.
+      eapply In_bvc_constrBind...
+Qed.
+
+Lemma notIn_bvb_bindsG : forall x b,
+    ~ In x (bvb b) ->
+    ~ In x (map fst (binds_Gamma b)).
+Proof with eauto.
+  intros.
+  destruct b.
+  all: simpl in H.
+  - destruct v.
+    simpl...
+  - destruct t.
+    simpl...
+  - destruct d.
+    destruct t.
+    simpl...
+    simpl in H...
+    intros Hcon.
+    apply Decidable.not_or in H.
+    destruct H.
+    destruct Hcon...
+    rewrite <- In_rev in H0.
+    eapply notIn_bvc_constrBind in H0.
+    rewrite map_rev in H1.
+    apply In_rev in H1.
+    eapply H0...
+Qed.
+
+Lemma In_bvbs_bindsG : forall bs x,
+    In x (bvbs bs) ->
+    In x (map fst (flatten (map binds_Gamma bs))).
+Proof with eauto.
+  induction bs. all: intros.
+  - simpl...
+  - simpl.
+    unfold flatten.
+    simpl.
+    rewrite concat_app.
+    simpl.
+    rewrite map_app.
+    apply in_or_app.
+    unfold bvbs in H.
+    simpl in H.
+    apply in_app_or in H.
+    destruct H...
+    + right.
+      rewrite app_nil_r.
+      apply In_bvb_bindsG...
+Qed.
+
+Lemma notIn_bvbs_bindsG : forall bs x,
+    ~ In x (bvbs bs) ->
+    ~ In x (map fst (flatten (map binds_Gamma bs))).
+Proof with eauto.
+  induction bs. all: intros.
+  - simpl...
+  - simpl.
+    unfold flatten.
+    simpl.
+    rewrite concat_app.
+    simpl.
+    rewrite map_app.
+    rewrite app_nil_r.
+    rewrite in_app_iff.
+    simpl in H.
+    unfold bvbs in H.
+    simpl in H.
+    rewrite in_app_iff in H.
+    apply Decidable.not_or in H.
+    destruct H.
+    intros Hcon.
+    destruct Hcon...
+    + eapply IHbs...
+    + eapply notIn_bvb_bindsG...
+Qed.
+
+Lemma In__map_normalise : forall l x ln,
+    In x (map fst l) ->
+    map_normalise l ln ->
+    In x (map fst ln).
+Proof with eauto.
+  induction l. all: intros.
+  - simpl in H. exfalso...
+  - inversion H0. subst.
+    destruct H. all: subst.
+    + simpl...
+    + simpl...
+Qed.
+
+Lemma notIn__map_normalise : forall l x ln,
+    ~ In x (map fst l) ->
+    map_normalise l ln ->
+    ~ In x (map fst ln).
+Proof with eauto.
+  induction l. all: intros.
+  - inversion H0. subst...
+  - inversion H0. subst.
+    simpl in H.
+    apply Decidable.not_or in H.
+    destruct H.
+    intros Hcon.
+    destruct Hcon...
+    eapply IHl...
+Qed.
+
+
+
+(** * Propositions *)
 
 Definition P_Term (t : Term) :=
   forall Delta Gamma x U Un v T,
@@ -18,64 +245,80 @@ Definition P_Binding (b : Binding) : Prop :=
     Delta ,, (x |-> U; Gamma) |-ok_b b ->
     normalise U Un ->
     empty ,, empty |-+ v : Un ->
-    Delta ,, Gamma |-ok_b <{ [v / x][b] b }> /\ 
-    binds_Delta b = binds_Delta <{ [v / x][b] b }> /\
-    binds_Gamma b = binds_Gamma <{ [v / x][b] b}>.
+    Delta ,, Gamma |-ok_b <{ [v / x][b] b }>.
 
 #[export] Hint Unfold
   P_Term
   P_Binding
   : core.
 
-Lemma P_letnonrec : forall bs t Delta Gamma x U Un v T,
-    P_Term t ->
-    Delta ,, (x |-> U; Gamma) |-+ (Let NonRec bs t) : T ->
-    normalise U Un ->
-    empty ,, empty |-+ v : Un ->
-    Util.ForallP P_Binding bs ->
-    Delta ,, Gamma |-+ <{ [v / x] {Let NonRec bs t} }> : T /\ 
-    List.map binds_Delta bs = List.map binds_Delta <{ [v / x][bnr] bs }> /\
-    List.map binds_Gamma bs = List.map binds_Gamma <{ [v / x][bnr] bs }>.
-Proof with (eauto with typing).
-  induction bs; intros.
-  - simpl.
-    split...
-    eapply T_Let...
-    + simpl.
-      eapply H...
-      inversion H0. subst.
-      simpl in H12.
-      simpl in H7.
-      inversion H7. subst.
-      eauto.
-    + inversion H0. subst...
-  - admit.
-Admitted.
 
-Lemma P_letrec : forall bs t Delta Gamma x U Un v T,
-    P_Term t ->
-    Delta ,, (x |-> U; Gamma) |-+ (Let Rec bs t) : T ->
-    normalise U Un ->
-    empty ,, empty |-+ v : Un ->
+
+(** * Binding sequences *)
+
+Lemma SPT__Bindings_NonRec : forall bs,
     Util.ForallP P_Binding bs ->
-    Delta ,, Gamma |-+ <{ [v / x] {Let Rec bs t} }> : T /\ 
-    List.map binds_Delta bs = List.map binds_Delta <{ [v / x][br] bs }> /\
-    List.map binds_Gamma bs = List.map binds_Gamma <{ [v / x][br] bs}>.
+    forall Delta Gamma x U Un v,
+      Delta ,, (x |-> U; Gamma) |-oks_nr bs ->
+      normalise U Un ->
+      empty ,, empty |-+ v : Un ->
+      Delta ,, Gamma |-oks_nr <{ [v / x][bnr] bs }>.
 Proof with (eauto with typing).
-  induction bs; intros.
+  induction bs. all: intros...
   - simpl.
-    split...
-    eapply T_LetRec...
-    + simpl.
-      eapply H...
-      inversion H0. subst.
-      simpl in H12.
-      simpl in H7.
-      inversion H7. subst.
-      eauto.
-    + inversion H0. subst...
-  - admit.
-Admitted.
+    inversion H0. subst.
+    destruct (existsb (eqb x) (bvb a)) eqn:Hexb.
+    + eapply W_ConsB_NonRec...
+      * eapply Util.ForallP_hd in H.
+        eapply H...
+      * rewrite subst_b__preserves_bindsG...
+      * rewrite subst_b__preserves_bindsD...
+        eapply existsb_exists in Hexb.
+        destruct Hexb as [x0 [HIn Heqb]].
+        apply eqb_eq in Heqb as Heq.
+        subst.
+        apply In_bvb_bindsG in HIn.
+        eapply In__map_normalise in H8...
+        rewrite mupdate_shadow_In in H9...
+    + eapply W_ConsB_NonRec...
+      * eapply Util.ForallP_hd in H.
+        eapply H...
+      * rewrite subst_b__preserves_bindsG...
+      * rewrite subst_b__preserves_bindsD...
+        eapply IHbs...
+        -- eapply Util.ForallP_tl...
+        -- eapply Util.existsb_nexists in Hexb.
+           assert (~ In x (bvb a)). {
+             intros Hcon.
+             apply Hexb.
+             exists x.
+             split...
+             apply eqb_refl.
+           }
+           apply notIn_bvb_bindsG in H3.
+           eapply notIn__map_normalise in H8...
+           rewrite mupdate_permute_In in H9...
+Qed.
+
+Lemma SPT__Bindings_Rec : forall bs,
+    Util.ForallP P_Binding bs ->
+    forall Delta Gamma x U Un v,
+      Delta ,, (x |-> U; Gamma) |-oks_r bs ->
+      normalise U Un ->
+      empty ,, empty |-+ v : Un ->
+      Delta ,, Gamma |-oks_r <{ [v / x][br] bs }>.
+Proof with (eauto with typing).
+  induction bs. all: intros...
+  - simpl.
+    inversion H0. subst.
+    eapply W_ConsB_Rec...
+    + eapply Util.ForallP_hd in H...
+    + eapply Util.ForallP_tl in H...
+Qed.
+
+
+
+(** * Main lemmas *)
 
 Lemma substitution_preserves_typing : 
   (forall t, P_Term t) /\ (forall b, P_Binding b).
@@ -85,12 +328,66 @@ Proof with eauto.
     (Q := P_Binding).
   all: intros; autounfold; intros.
   all: try solve [try (inversion H || inversion H0 || inversion H1); subst; eauto with typing].
-  - destruct rec.
+  - inversion H1. all: subst.
     + simpl.
-      all: eapply P_letnonrec; eauto.
+      destruct (existsb (eqb x) (bvbs bs)) eqn:Hexb.
+      * eapply T_Let...
+        -- rewrite subst_bnr__preserves_bindsG...
+        -- eapply SPT__Bindings_NonRec...
+        -- rewrite subst_bnr__preserves_bindsD...
+            eapply existsb_exists in Hexb.
+            destruct Hexb as [x0 [HIn Heqb]].
+            apply eqb_eq in Heqb as Heq.
+            subst.
+            apply In_bvbs_bindsG in HIn.
+            eapply In__map_normalise in HIn...
+            rewrite mupdate_shadow_In in H14...
+      * eapply T_Let...
+        -- rewrite subst_bnr__preserves_bindsG...
+        -- eapply SPT__Bindings_NonRec...
+        -- rewrite subst_bnr__preserves_bindsD...
+           eapply H0...
+            eapply Util.existsb_nexists in Hexb.
+            assert (~ In x (bvbs bs)). {
+              intros Hcon.
+              apply Hexb.
+              exists x.
+              split...
+              apply eqb_refl.
+            }
+            apply notIn_bvbs_bindsG in H4.
+            eapply notIn__map_normalise in H4...
+            rewrite mupdate_permute_In in H14...
     + simpl.
-      all: eapply P_letrec; eauto.
-  - simpl.
+      destruct (existsb (eqb x) (bvbs bs)) eqn:Hexb.
+      * eapply existsb_exists in Hexb.
+        destruct Hexb as [x0 [HIn Heqb]].
+        apply eqb_eq in Heqb as Heq.
+        subst.
+        apply In_bvbs_bindsG in HIn.
+        eapply In__map_normalise in HIn...
+        rewrite mupdate_shadow_In in H12...
+        rewrite mupdate_shadow_In in H14...
+        eapply T_LetRec...
+      * eapply Util.existsb_nexists in Hexb.
+        assert (~ In x (bvbs bs)). {
+          intros Hcon.
+          apply Hexb.
+          exists x.
+          split...
+          rewrite eqb_refl...
+        }
+        apply notIn_bvbs_bindsG in H4...
+        eapply notIn__map_normalise in H4...
+        rewrite mupdate_permute_In in H12...
+        rewrite mupdate_permute_In in H14...
+        eapply T_LetRec...
+        -- rewrite subst_br__preserves_bindsG...
+        -- rewrite subst_br__preserves_bindsD...
+            eapply SPT__Bindings_Rec...
+        -- rewrite subst_br__preserves_bindsD...
+  - (* Var *)
+    simpl.
     destruct (x =? s)%string eqn:Heqb.
     + apply eqb_eq in Heqb as Heq.
       subst.
@@ -116,7 +413,7 @@ Proof with eauto.
     + apply eqb_neq in Heqb as Hneq.
       apply T_LamAbs...
       rewrite update_permute in H11...
-Admitted.
+Qed.
 
 Corollary substitution_preserves_typing__Term : forall t Delta Gamma x U Un v T,
     Delta ,, (x |-> U; Gamma)  |-+ t : T ->
@@ -129,7 +426,5 @@ Corollary substitution_preserves_typing__Binding : forall b Delta Gamma x U Un v
     Delta ,, (x |-> U; Gamma) |-ok_b b ->
     normalise U Un ->
     empty ,, empty |-+ v : Un ->
-    Delta ,, Gamma |-ok_b <{ [v / x][b] b }> /\ 
-    binds_Delta b = binds_Delta <{ [v / x][b] b }> /\
-    binds_Gamma b = binds_Gamma <{ [v / x][b] b }>.
+    Delta ,, Gamma |-ok_b <{ [v / x][b] b }>.
 Proof. apply substitution_preserves_typing. Qed.
--- src/Util/Map/Mupdate.v
+++ src/Util/Map/Mupdate.v
@@ -37,8 +37,66 @@ Proof.
   - reflexivity.
   - destruct a. simpl. f_equal. auto.
 Qed.
-    
 
+Lemma mupdate_shadow : forall {X : Type} (xts : list (string * X)) (m : partial_map X) x v w,
+    (x |-> w; mupdate (x |-> v; m) xts) = (x |-> w; mupdate m xts).
+Proof with eauto.
+  induction xts. all: intros.
+  - simpl. apply update_shadow.
+  - simpl. 
+    destruct a as [y z].
+    destruct (x =? y)%string eqn:Heqb.
+    + apply eqb_eq in Heqb as Heq.
+      subst.
+      rewrite update_shadow.
+      rewrite update_shadow.
+      eauto.
+    + apply eqb_neq in Heqb as Hneq.
+      replace (x |-> w; y |-> z; mupdate (x |-> v; m) xts) with
+        (y |-> z; x |-> w; mupdate (x |-> v; m) xts) by eauto using update_permute.
+      replace (x |-> w; y |-> z; mupdate m xts) with
+        (y |-> z; x |-> w; mupdate m xts) by eauto using update_permute.
+      f_equal...
+Qed.
+
+Lemma mupdate_shadow_In : forall {X : Type} (xts : list (string * X)) (m : partial_map X) x v,
+    In x (map fst xts) ->
+    mupdate (x |-> v ; m) xts = mupdate m xts.
+Proof with eauto.
+  induction xts. all: intros.
+  - inversion H.
+  - simpl.
+    simpl in H.
+    destruct a as [y w].
+    destruct H.
+    + simpl in H.
+      subst.
+      apply mupdate_shadow.
+    + f_equal...
+Qed.
+
+Lemma mupdate_permute_In : forall {X : Type} (xts : list (string * X)) (m : partial_map X) x v,
+    ~ In x (map fst xts) ->
+    mupdate (x |-> v; m) xts = (x |-> v; mupdate m xts).
+Proof with eauto.
+  induction xts. all: intros.
+  - reflexivity.
+  - simpl.
+    simpl in H.
+    destruct a as [y w].
+    destruct (x =? y)%string eqn:Heqb.
+    + apply eqb_eq in Heqb.
+      subst.
+      exfalso...
+    + apply eqb_neq in Heqb.
+      rewrite update_permute...
+      f_equal.
+      apply IHxts.
+      intros Hcon.
+      apply H.
+      right...
+Qed.
+    
 Lemma inclusion_mupdate : forall {X : Type} (m m' : partial_map X) xts,
   inclusion m m' ->
   inclusion (mupdate m xts) (mupdate m' xts).
