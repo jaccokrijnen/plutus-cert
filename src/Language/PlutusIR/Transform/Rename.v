10381bbaa5bcf1cb3d7dda94d37ecd883ee8a4aa
Jacco Krijnen <j.o.g.krijnen@uu.nl>
2022-11-02 12:39:08 +0100

    Some fixes in rename
    * Write correct no_capture hypothesis in rn_LamAbs and rn_TyAbs
    * Check no_capture_ty in constructor types of ADT

--- src/Language/PlutusIR/Transform/Rename.v
+++ src/Language/PlutusIR/Transform/Rename.v
@@ -25,25 +25,26 @@ Definition ctx := list (string * string).
 
 (* Binding variable x does not capture free variables in t if they were renamed
    according to Γ or Δ *)
-Definition no_capture (Γ : ctx) x t :=
+Definition no_capture x (Γ : ctx) t :=
   forall y, In (y, x) Γ -> ~ AFI.Term.appears_free_in y t.
 
-Definition no_captureA (Δ : ctx) α t :=
+Definition no_captureA α (Δ : ctx) t :=
   forall β, In (β, α) Δ -> ~ AFI.Annotation.appears_free_in β t.
 
-Definition no_ty_capture (Δ : ctx) α τ :=
+Definition no_ty_capture α (Δ : ctx) τ :=
   forall β, In (β, α) Δ -> ~ AFI.Ty.appears_free_in β τ.
 
 
-Inductive rename_tvs (Δ : ctx) : list TVDecl -> list TVDecl -> ctx -> Type :=
+Inductive rename_tvs (Δ : ctx) (cs : list constructor) : list TVDecl -> list TVDecl -> ctx -> Type :=
 
   | rn_tvs_nil :
-      rename_tvs Δ [] [] []
+      rename_tvs Δ cs [] [] []
 
   | rn_tvs_cons : forall α tvs k β tvs' Δ_tvs,
-      lookup α Δ = Some β ->
-      rename_tvs Δ tvs tvs' Δ_tvs ->
-      rename_tvs Δ (TyVarDecl α k :: tvs) (TyVarDecl β k :: tvs') ((α, β) :: Δ_tvs)
+      (* no_capture in any of the constructor types *)
+      Forall (fun '(Constructor (VarDecl _ cty) _) => no_ty_capture β Δ cty) cs ->
+      rename_tvs ((α, β) :: Δ) cs tvs tvs' Δ_tvs ->
+      rename_tvs Δ cs (TyVarDecl α k :: tvs) (TyVarDecl β k :: tvs') ((α, β) :: Δ_tvs)
 .
 
 Inductive rename_ty (Δ : ctx) : Ty -> Ty -> Type :=
@@ -64,7 +65,7 @@ Inductive rename_ty (Δ : ctx) : Ty -> Ty -> Type :=
 
    | rn_Ty_Forall : forall α α' k τ τ',
       rename_ty ((α, α') :: Δ) τ τ' ->
-      no_ty_capture Δ α τ ->
+      no_ty_capture α Δ τ ->
       rename_ty Δ (Ty_Forall α k τ) (Ty_Forall α' k τ')
 
    | rn_Ty_Builtin : forall t,
@@ -72,7 +73,7 @@ Inductive rename_ty (Δ : ctx) : Ty -> Ty -> Type :=
 
    | rn_Ty_Lam : forall α α' k τ τ',
       rename_ty ((α, α') :: Δ) τ τ' ->
-      no_ty_capture Δ α τ ->
+      no_ty_capture α Δ τ ->
       rename_ty Δ (Ty_Lam α k τ) (Ty_Lam α' k τ')
 
    | Ty_App : forall σ τ σ' τ',
@@ -95,8 +96,8 @@ Inductive rename (Γ Δ : ctx) : Term -> Term -> Type :=
 
          Alternatively, add `Let NonRec bs t` as index in rename_binding 
          and put a simple no_capture at the actual binding *)
-      Forall (fun '(_, x) => no_capture Γ x t) Γ_bs ->
-      Forall (fun '(_, α) => no_captureA Δ α t) Δ_bs ->
+      Forall (fun '(_, x') => no_capture x' Γ t) Γ_bs ->
+      Forall (fun '(_, α') => no_captureA α' Δ t) Δ_bs ->
 
       (* All bound (type) variables have to be unique in the binding group *)
       NoDup (bvbs bs') ->
@@ -114,24 +115,24 @@ Inductive rename (Γ Δ : ctx) : Term -> Term -> Type :=
       rename_binding Γ Δ Γ_b Δ_b b b' ->
       rename (Γ_b ++ Γ) (Δ_b ++ Δ) (Let NonRec bs t) (Let NonRec bs' t') ->
 
-      (* All bound (type) variables in the bounding should not capture.
+      (* All bound (type) variables in the let should not capture.
 
          Alternatively, add `Let NonRec bs t` as index in rename_binding 
          and put a simple no_capture at the actual binding *)
-      Forall (fun '(_, x) => no_capture Γ x (Let NonRec bs t)) Γ_b ->
-      Forall (fun '(_, α) => no_captureA Δ α (Let NonRec bs t)) Δ_b ->
+      Forall (fun '(_, x') => no_capture x' Γ (Let NonRec bs t)) Γ_b ->
+      Forall (fun '(_, α') => no_captureA α' Δ (Let NonRec bs t)) Δ_b ->
 
       rename Γ Δ (Let NonRec (b :: bs) t) (Let NonRec (b' :: bs') t')
 
   | rn_TyAbs : forall α α' k t t',
       rename ((α, α') :: Γ) Δ t t' ->
-      no_captureA Δ α t ->
+      no_captureA α' Δ t ->
       rename Γ Δ (TyAbs α k t) (TyAbs α' k t')
 
   | rn_LamAbs : forall x x' τ τ' t t',
       rename_ty Δ τ τ' ->
       rename ((x, x') :: Γ) Δ t t' ->
-      no_capture Δ x t ->
+      no_capture x' Δ t ->
       rename Γ Δ (LamAbs x τ t) (LamAbs x' τ' t')
 
   | rn_Apply : forall s t s' t',
@@ -177,10 +178,17 @@ with rename_binding (Γ Δ : ctx) : ctx -> ctx -> Binding -> Binding -> Type :=
 
   | rn_DatatypeBind : forall α α' k tvs tvs' elim elim' cs cs',
       forall Δ_tvs Γ_cs Γ_b Δ_b,
+
+      (* Renamings of ty-vars, used in constructor types *)
       rename_tvs Δ tvs tvs' Δ_tvs ->
-      rename_constrs Γ Δ Γ_cs cs cs' ->
+      (* Constructor types are renamed and return any renamed constructor names *)
+      rename_constrs Γ ((α, α') :: Γ_tvs ++ Δ) Γ_cs cs cs' ->
+
+      (* Renamings for the rest of the program *)
       Γ_b = (elim, elim') :: Γ_cs ->
-      Δ_b = (α, α') :: Δ_tvs ->
+      Δ_b = [(α, α')] ->
+
+
       rename_binding Γ Δ Γ_b Δ_b
         (DatatypeBind (Datatype (TyVarDecl α k) tvs elim cs))
         (DatatypeBind (Datatype (TyVarDecl α' k) tvs' elim' cs'))
