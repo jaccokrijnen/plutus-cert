0185982bdec0ca88889bdbdc5e151def006f611c
Jacco Krijnen <j.o.g.krijnen@uu.nl>
2022-11-11 14:16:29 +0100

    Add not-working coercions and remove mutually recursive ADT

--- src/Language/PlutusIR/Transform/Identity.v
+++ src/Language/PlutusIR/Transform/Identity.v
@@ -18,6 +18,9 @@ Open Scope string_scope.
 Inductive three :=
   One : Term -> three | Two : Binding -> three | Three : list Binding -> three.
 
+Coercion One : Term >-> three.
+Coercion Two : Binding >-> three.
+
 Inductive identity : three -> three -> Prop :=
   | rn_Var : forall x,
       identity (One (Var x)) (One (Var x))
@@ -43,42 +46,7 @@ Inductive identity : three -> three -> Prop :=
 From QuickChick Require Import QuickChick.
 
 Derive DecOpt for (identity t t').
-Instance well_scoped_sound ctx ty : DecOptSoundPos (well_scoped_Ty ctx ty).
-Proof.
-derive_sound.
-Qed.
-
-
-Inductive identity : Term -> Term -> Type :=
-  | rn_Var : forall x,
-      identity (Var x) (Var x)
-
-  | rn_Let_Rec : forall r bs bs' t t',
-      identity t t' ->
-      identity_bindings bs bs' ->
-      identity (Let r bs t) (Let r bs' t')
-
-with identity_binding : Binding -> Binding -> Type :=
-
-  | rn_TermBind : forall s x Ty_Var "a" t t',
-      identity t t' ->
-      identity_binding (TermBind s (VarDecl x Ty_Var "a") t) (TermBind s (VarDecl x Ty_Var "a") t')
-
-with identity_bindings : list Binding -> list Binding -> Type :=
-
-  | rn_Bindings_Rec_nil :
-      identity_bindings [] []
-
-  | rn_Bindings_Rec_cons : forall b b' bs bs',
-      identity_binding b b' ->
-      identity_bindings bs bs' ->
-      identity_bindings (b :: bs) (b' :: bs')
-  .
-
-From QuickChick Require Import QuickChick.
-
-Derive DecOpt for (identity t t').
-Instance well_scoped_sound ctx ty : DecOptSoundPos (well_scoped_Ty ctx ty).
+Instance identity_sound t t' : DecOptSoundPos (identity t t').
 Proof.
 derive_sound.
 Qed.
