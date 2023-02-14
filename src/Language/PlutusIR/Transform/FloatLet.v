9ceb60ccc5d7bd7cba82fed746cb6d33378fdf61
xiving <b.f.remmers@students.uu.nl>
2023-02-14 10:55:40 +0100

    Moved unique out of submodules, and added DecOpt instances

--- src/Language/PlutusIR/Transform/FloatLet.v
+++ src/Language/PlutusIR/Transform/FloatLet.v
@@ -241,7 +241,7 @@ Inductive transitive_closure (R : Term -> Term -> Prop) : Term -> Term -> Prop :
 
 
 Definition let_float t_pre t_post
-  := Term.unique t_pre
+  := unique_tm t_pre
   /\ closed t_post
   /\ exists t' t'',
     (  transitive_closure let_float_step t_pre t'
