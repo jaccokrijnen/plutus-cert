9dc240d3068d53a6503111d534e124d7aff75b6f
Jacco Krijnen <j.o.g.krijnen@uu.nl>
2023-04-20 11:30:53 +0200

    Add Constr to value, fix lemmas

--- src/Language/PlutusIR/Semantics/Dynamic/Bigstep/EvalValue.v
+++ src/Language/PlutusIR/Semantics/Dynamic/Bigstep/EvalValue.v
@@ -9,13 +9,16 @@ Lemma eval_value :
   (forall v, value v -> v =[0]=> v) /\
   (forall n v, neutral_value n v -> v =[0]=> v).
 Proof with (autounfold; (eauto || (try lia))).
-  apply value__multind.
+  apply value___multind.
   all: intros.
-  3: {
   all: try solve [constructor; eauto].
-  - inversion H; subst.
-    + econstructor.
-    + eapply E_Constr_cons.
+
+  (* Constr*)
+  - induction vs.
+    + constructor.
+    + inversion H. inversion H0. subst.
+      eapply E_Constr_cons with (k_t := 0) (k_ts := 0)...
+  - assumption.
   - inversion H2; subst.
     all: econstructor...
     all: econstructor...
--- src/Language/PlutusIR/Semantics/Dynamic/Values.v
+++ src/Language/PlutusIR/Semantics/Dynamic/Values.v
@@ -67,6 +67,61 @@ Combined Scheme value__multind from
   value__ind,
   neutral_value__ind.
 
+Section value_multind.
+
+Context
+  (P : Term -> Prop)
+  (Q : nat -> Term -> Prop).
+
+Context
+  (H_LamAbs : forall (x : String.string) (T : Ty) (t0 : Term), P (LamAbs x T t0))
+  (H_TyAbs : forall (X : String.string) (K : Kind) (t : Term), P (TyAbs X K t))
+  (H_IWrap : forall (F T : Ty) (v : Term), value v -> P v -> ~ is_error v -> P (IWrap F T v))
+  (H_Constant : forall u : some valueOf, P (Constant u))
+  (H_Error : forall T : Ty, P (Error T))
+  (H_Constr : forall (i : nat) (vs : list Term), Forall value vs -> Forall P vs -> P (Constr i vs))
+  (H_Neutral : forall nv : Term, neutral_value 0 nv -> Q 0 nv -> P nv)
+  (H_Builtin : forall (n : nat) (f6 : DefaultFun), n < arity f6 -> Q n (Builtin f6))
+  (H_Apply : forall (n : nat) (nv v : Term), value v -> P v -> ~ is_error v ->
+        neutral_value (S n) nv -> Q (S n) nv -> Q n (Apply nv v))
+  (H_TyInst : forall (n : nat) (nv : Term) (T : Ty),
+        neutral_value (S n) nv -> Q (S n) nv -> Q n (TyInst nv T)).
+
+Fixpoint values__ind (H_value : forall (t : Term), value t -> P t) (ts : list Term) (vs : Forall value ts): Forall P ts :=
+  match vs as vs in Forall _ ts return Forall P ts with
+    | Forall_nil _ => Forall_nil _
+    | @Forall_cons _ _ x xs vx vxs => @Forall_cons _ _ x xs (H_value x vx) (values__ind H_value xs vxs)
+  end.
+
+Fixpoint value___ind (t : Term) (v : value t) {struct v} : P t :=
+  match v in (value t0) return (P t0) with
+  | V_LamAbs x T t0 => H_LamAbs x T t0
+  | V_TyAbs X K t0 => H_TyAbs X K t0
+  | V_IWrap F1 T v0 v1 n => H_IWrap F1 T v0 v1 (value___ind v0 v1) n
+  | V_Constant u => H_Constant u
+  | V_Error T => H_Error T
+  | V_Constr i ts vs => H_Constr i ts vs
+      ((fix F ts vs := match vs as vs in Forall _ ts return Forall P ts with
+        | Forall_nil _ => Forall_nil _
+        | @Forall_cons _ _ t ts vt vts => @Forall_cons _ _ t ts (value___ind t vt) (F ts vts)
+      end) ts vs)
+
+  | V_Neutral nv n => H_Neutral nv n (neutral___ind 0 nv n)
+  end
+with neutral___ind (n : nat) (t : Term) (n0 : neutral_value n t) {struct n0} : Q n t :=
+  match n0 in (neutral_value n1 t0) return (Q n1 t0) with
+  | NV_Builtin n1 f9 l => H_Builtin n1 f9 l
+  | NV_Apply n1 nv v v0 n2 n3 =>
+      H_Apply n1 nv v v0 (value___ind v v0) n2 n3 (neutral___ind (S n1) nv n3)
+  | NV_TyInst n1 nv T n2 => H_TyInst n1 nv T n2 (neutral___ind (S n1) nv n2)
+  end
+.
+
+Combined Scheme value___multind from value___ind, neutral___ind.
+
+
+End value_multind.
+
 Definition neutral (t : Term) := neutral_value 0 t.
 
 #[export] Hint Unfold neutral : core.
