7d40d7967fb694bd677128fe21622995c709df41
Jacco Krijnen <j.o.g.krijnen@uu.nl>
2024-03-04 15:24:12 +0100

    WIP on dead-code Decide2.v

--- src/PlutusIR/Transform/DeadCode/Decide2.v
+++ src/PlutusIR/Transform/DeadCode/Decide2.v
@@ -4,6 +4,7 @@ Require Import
 .
 From PlutusCert Require Import
   Util
+  Util.Tactics
   PlutusIR
   PlutusIR.Analysis.Equality
   DeadCode2
@@ -19,34 +20,30 @@ Import ListNotations.
 Require Import
   Arith.
 
+Definition dec_unused_in (b : Binding) (t' : Term) : bool :=
+  forallb (fun x => negb (existsb (String.eqb x) (fv t'))) (bvb b) &&
+  forallb (fun x => negb (existsb (String.eqb x) (ftv t'))) (btvb b)
+.
+
 Section Bindings.
 
   Context (dec_Term : Term -> Term -> bool).
 
-  Definition dec_Binding (b b' : Binding) : bool := match b, b' with
-    | TermBind s vd t, TermBind s' vd' t' => Strictness_eqb s s' && VDecl_eqb vd vd' && dec_Term t t'
-    | b, b' => Binding_eqb b b'
-    end.
-
-  Definition unused_in (b : Binding) (t' : Term) : bool :=
-    forallb (fun x => negb (existsb (String.eqb x) (fv t'))) (bvb b) &&
-    forallb (fun x => negb (existsb (String.eqb x) (ftv t'))) (btvb b)
-  .
 
   (* t' is the post-term let body *)
-  Fixpoint dec_Bindings_NonRec (bs bs' : list Binding) (t' : Term) : bool :=
+  Function dec_Bindings_NonRec (bs bs' : list Binding) (t' : Term) : bool :=
     match bs, bs' with
     | b :: bs, b' :: bs' =>
-        if dec_Binding b b'
+        if dec_compat_binding dec_Term b b'
         then dec_Bindings_NonRec bs bs' t'
         else
           dec_pure_binding [] b &&
-          unused_in b (Let NonRec bs' t') &&
+          dec_unused_in b (Let NonRec bs' t') &&
           dec_Bindings_NonRec bs (b' :: bs') t'
     | [], [] => true
     | b :: bs, [] =>
           dec_pure_binding [] b &&
-          unused_in b t' &&
+          dec_unused_in b t' &&
           dec_Bindings_NonRec bs [] t'
     | [], _ => (* Left-over bindings in post-term, impossible *)
         false
@@ -54,11 +51,60 @@ Section Bindings.
 
 End Bindings.
 
-Fixpoint dec_Term (t t' : Term) {struct t} :=
+Function dec_Term (t t' : Term) {struct t} :=
   match t, t' with
   | Let NonRec bs t, Let NonRec bs' t' =>
       if dec_Bindings_NonRec dec_Term bs bs' t'
       then dec_Term t t'
       else false
   | _, _ => dec_compat dec_Term t t'
-  end.
+  end
+.
+
+Lemma unused_sound b t' : dec_unused_in b t' = true -> unused_in b t'.
+Admitted.
+
+Lemma pure_sound b : dec_pure_binding [] b = true -> pure_binding [] b.
+Admitted.
+
+Lemma dec_Bindings_NonRec_sound bs bs' t'
+  (dec_Term_sound : forall t t', dec_Term t t = true -> dc t t')
+  : dec_Bindings_NonRec dec_Term bs bs' t' = true
+  -> dc_NonRec t' bs bs'.
+Admitted.
+
+Definition P_Term := fun t => forall t', dec_Term t t' = true -> dc t t'.
+Definition P_Binding := fun b => forall b', dec_compat_binding dec_Term b b' = true -> Compat_Binding dc b b'.
+
+Lemma dec_Term_sound : forall t, P_Term t.
+Proof.
+  apply Term__ind with (P := P_Term) (Q := P_Binding).
+  all: intros.
+  all: unfold P_Term, P_Binding.
+
+  (* First do all the Term cases *)
+  all: match goal with
+    | |- (forall (_ : Binding), _) => shelve
+    | _ => idtac
+    end.
+  all: intros t'; destruct t'.
+
+  (* Solve impossible cases *)
+  all: rewrite dec_Term_equation.
+  all: try repeat destruct_match.
+  all: try inversion 1.
+
+  - (* Let NonRec *) admit.
+  - destruct r; intuition.
+    destruct_hypos.
+    repeat constructor.
+    unfold P_Binding in *.
+    all: admit. (* should hold from IH's*)
+  - admit.
+  - destruct_hypos.
+    + 
+    eauto using dc.
+    constructor.
+    admit.
+  - all: admit.
+Admitted.
--- src/Util/Tactics.v
+++ src/Util/Tactics.v
@@ -1,4 +1,18 @@
+Require Import Bool.
+
 Ltac destruct_hypos := repeat (match goal with
   | H : exists a, _ |- _ => destruct H
   | H : ?x /\ ?y |- _ => destruct H
+  | H : ?x && ?y = true |- _ => apply andb_prop in H
   end).
+
+Ltac destruct_if :=
+      match goal with
+        | |- context[if ?X then _ else _] => destruct X eqn:H_eqb
+      end.
+
+Ltac destruct_match :=
+      match goal with
+      | |- context[match ?X with | _ => _ end] => destruct X eqn:?
+      end.
+
