a5ea1474859935ab70c7b2d9dfe6266ad8aab439
TheCodingWombat <richardkoetschruyter@gmail.com>
2024-08-22 11:20:19 +0200

    Comment on main normalizer_sound theorem

--- src/PlutusIR/Semantics/Static/normaliser/Normalisation_STLC_named_sound.v
+++ src/PlutusIR/Semantics/Static/normaliser/Normalisation_STLC_named_sound.v
@@ -1,6 +1,9 @@
 From PlutusCert Require Import Normalisation_STLC_named.
 From PlutusCert Require Import Normalizer_STLC_named.
 
+(* Running normalizer on named STLC results in a value that
+    satisfies the normalise relation *)
+
 Theorem normalizer_named_sound : forall t T v H_wt,
     normalizer T t H_wt = v -> normalise t v.
 Proof.
