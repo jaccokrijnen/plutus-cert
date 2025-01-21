a16973482a3bf743f1c3938cda28c24a62905e80
TheCodingWombat <richardkoetschruyter@gmail.com>
2025-01-21 08:52:22 +0100

    Add btv condition to TyAbs rule

--- src/PlutusIR/Semantics/Static/Typing.v
+++ src/PlutusIR/Semantics/Static/Typing.v
@@ -80,6 +80,7 @@ Inductive has_type : list (string * kind) -> list (string * ty) -> term -> ty ->
   (* Universal types *)
   | T_TyAbs : forall Δ Γ X K t Tn,
       ((X, K) :: Δ) ,, Γ |-+ t : Tn ->
+      ~ In X (BoundVars.btv t) ->
       Δ ,, Γ |-+ (TyAbs X K t) : (Ty_Forall X K Tn)
   | T_TyInst : forall Δ Γ t1 T2 T1n X K2 T0n T2n,
       Δ ,, Γ |-+ t1 : (Ty_Forall X K2 T1n) ->
