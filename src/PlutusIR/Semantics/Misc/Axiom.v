3b9526cbdecc117024fb8a57f202edb465bd4e68
Jacco Krijnen <j.o.g.krijnen@uu.nl>
2025-04-09 11:22:28 +0200

    Remove unused axiom uniqueness

--- src/PlutusIR/Semantics/Misc/Axiom.v
+++ src/PlutusIR/Semantics/Misc/Axiom.v
@@ -4,8 +4,6 @@ Require Export PlutusCert.PlutusIR.Analysis.BoundVars.
 Require Export PlutusCert.PlutusIR.Analysis.UniqueBinders.
 Require Import PlutusCert.PlutusIR.Semantics.Static.
 
-Axiom uniqueness : forall t, unique_tm t.
-
 Axiom uniqueness' : forall (Delta : Delta) e,
   forall X,
     appears_bound_in_ann X e ->
