3764b9415ad0b79838956345325a9dd939bb6226
TheCodingWombat <richardkoetschruyter@gmail.com>
2025-05-01 09:39:31 +0200

    Kinding preservation

--- src/PlutusIR/Semantics/TypeSafety/TypeLanguage/Preservation.v
+++ src/PlutusIR/Semantics/TypeSafety/TypeLanguage/Preservation.v
@@ -2,11 +2,57 @@ Require Import PlutusCert.PlutusIR.Semantics.Static.
 Require Import PlutusCert.PlutusIR.Semantics.Dynamic.
 Require Import PlutusCert.PlutusIR.Semantics.TypeSafety.SubstitutionPreservesTyping.
 
-Theorem preservation : forall Delta T K Tn,
-    Delta |-* T : K ->
+From Coq Require Import Program.Equality.
+
+Theorem preservation : forall Δ T K Tn,
+    Δ |-* T : K ->
     normalise T Tn ->
-    Delta |-* Tn : K.
+    Δ |-* Tn : K.
 Proof.
-(* Richard: I have proof sketches for this in normalisation/type checking files *)
-(* ADMIT: I had no time to finish this. Should hold according to papers. *)
+    intros Δ T K Tn Hkind_T Hnorm.
+    generalize dependent K.
+    generalize dependent Δ.
+    induction Hnorm; intros.
+    - (* N_BetaReduce *)
+      dependent destruction Hkind_T; subst.
+      eapply IHHnorm3.
+      eapply substituteTCA_preserves_kinding.
+      + eapply IHHnorm1 in Hkind_T1.
+        inversion Hkind_T1; subst.
+        eauto.
+      + eapply IHHnorm2; eauto.
+    - (* N_TyApp *)
+      dependent destruction Hkind_T; subst.
+      econstructor.
+      + eapply IHHnorm1; eauto.
+      + eapply IHHnorm2; eauto. 
+    - (* N_TyFun *)
+      dependent destruction Hkind_T; subst.
+      econstructor.
+      + eapply IHHnorm1; eauto.
+      + eapply IHHnorm2; eauto.
+    - (* N_TyForall *)
+      dependent destruction Hkind_T; subst.
+      econstructor.
+      eapply IHHnorm; eauto.
+    - (* N_TyLam *)
+        dependent destruction Hkind_T; subst.
+        econstructor.
+        eapply IHHnorm; eauto.
+    - (* N_TyVar *)
+        dependent destruction Hkind_T; subst.
+        constructor.
+        assumption.
+    - (* N_TyIFix *)
+        dependent destruction Hkind_T; subst.
+        econstructor.
+        + eapply IHHnorm2; eauto.
+        + eapply IHHnorm1; eauto.
+    - (* N_TyBuiltin *)
+        dependent destruction Hkind_T; subst.
+        constructor.
+        assumption.
+    - (* N_TySOP *) 
+       (* ADMIT: TySOP unimplemented *)
+       admit.
 Admitted.
