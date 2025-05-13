66bf495844c3a03d04247aa71dd55d5f5883eb56
TheCodingWombat <richardkoetschruyter@gmail.com>
2025-05-13 09:38:44 +0200

    Fill out StrongNormalisation.v

--- src/PlutusIR/Semantics/TypeSafety/TypeLanguage/StrongNormalisation.v
+++ src/PlutusIR/Semantics/TypeSafety/TypeLanguage/StrongNormalisation.v
@@ -1,10 +1,22 @@
 Require Import PlutusCert.PlutusIR.Semantics.Static.
-Require Import PlutusCert.PlutusIR.Semantics.Dynamic.
-Require Import PlutusCert.PlutusIR.Semantics.TypeSafety.SubstitutionPreservesTyping.
+Require Import PlutusCert.PlutusIR.Semantics.Static.Kinding.Checker.
 
-Theorem strong_normalisation : forall Delta T K,
-    Delta |-* T : K ->
+Require Import PlutusCert.PlutusIR.Semantics.Static.Normalisation.Norm_sound_complete.
+From Coq Require Import ssreflect.
+
+From PlutusCert Require Import port_plut2.
+
+Theorem strong_normalisation Δ T K :
+    Δ |-* T : K ->
     exists Tn, normalise T Tn.
 Proof.
-(* ADMIT: I had no time to finish this. Should hold according to papers. *)
-Admitted.
+    intros Hkind.
+    remember Hkind as Hkind'. clear HeqHkind'.
+    apply plutus_ty_strong_normalization in Hkind.
+    assert ({Tn & normalise T Tn}) as [Tn Hnorm].
+    {
+        eapply SN_normalise; eauto.
+    }
+    exists Tn.
+    assumption.
+Qed.
