f8824c3b0fadbb96608ef45d5908205b6012c7d7
Jacco <j.o.g.krijnen@uu.nl>
2025-05-14 17:41:30 +0200

    Use nix flake/direnv without forked QuickChick

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  # pin to 24.11
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/5b35d248e9206c1f3baf8de6a7683fee126364aa";
+
+  outputs = inputs : {
+    devShells.x86_64-linux.default =
+      let pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+      in
+      pkgs.mkShell {
+        buildInputs = with pkgs; [
+          coq
+          coqPackages.mathcomp
+          coqPackages.ExtLib
+          coqPackages.QuickChick
+          coqPackages.equations
+          ];
+      };
+  };
+}
