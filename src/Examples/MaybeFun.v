ad8e92a095080b19a0b7197064a5f53719057a26
Jacco <j.o.g.krijnen@uu.nl>
2025-05-27 16:45:49 +0200

    Change MaybeFun example so it has no escaping in top-level type

--- src/Examples/MaybeFun.v
+++ src/Examples/MaybeFun.v
@@ -5,65 +5,122 @@ Import Parser.DumpNotations.
 Open Scope pir_dump_scope.
 
 (*
-Last PIR term when compiling maybeFun in (Test.Optimization.Spec.hs)
 
-maybeFun :: CompiledCode (Maybe Integer -> Maybe Integer -> Maybe Integer)
-maybeFun = $$(compile
-   [|| \(x :: Maybe Integer) (y :: Maybe Integer) ->
-         case x of
-            Just x' -> case y of
-                 Just y' -> Just (x' `Builtins.addInteger` y')
-                 Nothing -> Nothing
-            Nothing -> Nothing
+Adapted the maybeFun from (plutus-tx-plugin/test/Optimization/Spec.hs), so that
+it has no escaping in its top-level type (maybeFun has Maybe in its top-level type).
+
+maybeFun' :: CompiledCode Integer
+maybeFun' = $$(compile
+   [|| let f = \(x :: Maybe Integer) (y :: Maybe Integer) ->
+                case x of
+                   Just x' -> case y of
+                        Just y' -> Just (x' `Builtins.addInteger` y')
+                        Nothing -> Nothing
+                   Nothing -> Nothing
+        in case f (Just 3) (Just 5) of
+          Just n -> n
+          Nothing -> 0
    ||])
 
 *)
 
-Definition maybeFun_compiled :=
+Definition maybeFun__dead_code_eliminated:=
 
-  Let () NonRec (DatatypeBind () (Datatype () (TyVarDecl () (TyName (Name
-  [x4d,x61,x79,x62,x65] (Unique 1619))) (KindArrow () (Type ()) (Type ())))
-  [TyVarDecl () (TyName (Name [x61] (Unique 1623))) (Type ())] (Name
-  [x4d,x61,x79,x62,x65,x5f,x6d,x61,x74,x63,x68] (Unique 1622)) [VarDecl () (Name
-  [x4a,x75,x73,x74] (Unique 1620)) (TyFun () (TyVar () (TyName (Name [x61]
-  (Unique 1623)))) (TyApp () (TyVar () (TyName (Name [x4d,x61,x79,x62,x65]
-  (Unique 1619)))) (TyVar () (TyName (Name [x61] (Unique 1623)))))),VarDecl ()
-  (Name [x4e,x6f,x74,x68,x69,x6e,x67] (Unique 1621)) (TyApp () (TyVar () (TyName
-  (Name [x4d,x61,x79,x62,x65] (Unique 1619)))) (TyVar () (TyName (Name [x61]
-  (Unique 1623)))))]) :| []) (LamAbs () (Name [x64,x73] (Unique 1624)) (TyApp ()
-  (TyVar () (TyName (Name [x4d,x61,x79,x62,x65] (Unique 1619)))) (TyBuiltin ()
-  (SomeTypeIn DefaultUniInteger))) (LamAbs () (Name [x64,x73] (Unique 1625))
-  (TyApp () (TyVar () (TyName (Name [x4d,x61,x79,x62,x65] (Unique 1619))))
-  (TyBuiltin () (SomeTypeIn DefaultUniInteger))) (TyInst () (Apply () (Apply ()
-  (TyInst () (Apply () (TyInst () (Var () (Name
-  [x4d,x61,x79,x62,x65,x5f,x6d,x61,x74,x63,x68] (Unique 1622))) (TyBuiltin ()
-  (SomeTypeIn DefaultUniInteger))) (Var () (Name [x64,x73] (Unique 1624))))
-  (TyForall () (TyName (Name [x64,x65,x61,x64] (Unique 1626))) (Type ()) (TyApp
-  () (TyVar () (TyName (Name [x4d,x61,x79,x62,x65] (Unique 1619)))) (TyBuiltin
-  () (SomeTypeIn DefaultUniInteger))))) (LamAbs () (Name [x78,x27] (Unique
-  1627)) (TyBuiltin () (SomeTypeIn DefaultUniInteger)) (TyAbs () (TyName (Name
-  [x64,x65,x61,x64] (Unique 1628))) (Type ()) (TyInst () (Apply () (Apply ()
-  (TyInst () (Apply () (TyInst () (Var () (Name
-  [x4d,x61,x79,x62,x65,x5f,x6d,x61,x74,x63,x68] (Unique 1622))) (TyBuiltin ()
-  (SomeTypeIn DefaultUniInteger))) (Var () (Name [x64,x73] (Unique 1625))))
-  (TyForall () (TyName (Name [x64,x65,x61,x64] (Unique 1629))) (Type ()) (TyApp
-  () (TyVar () (TyName (Name [x4d,x61,x79,x62,x65] (Unique 1619)))) (TyBuiltin
-  () (SomeTypeIn DefaultUniInteger))))) (LamAbs () (Name [x79,x27] (Unique
-  1630)) (TyBuiltin () (SomeTypeIn DefaultUniInteger)) (TyAbs () (TyName (Name
-  [x64,x65,x61,x64] (Unique 1631))) (Type ()) (Apply () (TyInst () (Var () (Name
-  [x4a,x75,x73,x74] (Unique 1620))) (TyBuiltin () (SomeTypeIn
-  DefaultUniInteger))) (Apply () (Apply () (Builtin () AddInteger) (Var () (Name
-  [x78,x27] (Unique 1627)))) (Var () (Name [x79,x27] (Unique 1630)))))))) (TyAbs
-  () (TyName (Name [x64,x65,x61,x64] (Unique 1632))) (Type ()) (TyInst () (Var
-  () (Name [x4e,x6f,x74,x68,x69,x6e,x67] (Unique 1621))) (TyBuiltin ()
-  (SomeTypeIn DefaultUniInteger))))) (TyForall () (TyName (Name
-  [x64,x65,x61,x64] (Unique 1633))) (Type ()) (TyVar () (TyName (Name
-  [x64,x65,x61,x64] (Unique 1633))))))))) (TyAbs () (TyName (Name
-  [x64,x65,x61,x64] (Unique 1634))) (Type ()) (TyInst () (Var () (Name
-  [x4e,x6f,x74,x68,x69,x6e,x67] (Unique 1621))) (TyBuiltin () (SomeTypeIn
-  DefaultUniInteger))))) (TyForall () (TyName (Name [x64,x65,x61,x64] (Unique
-  1635))) (Type ()) (TyVar () (TyName (Name [x64,x65,x61,x64] (Unique
-  1635))))))))
+Let () NonRec (TermBind () Strict (VarDecl () (Name
+[x61,x64,x64,x49,x6e,x74,x65,x67,x65,x72] (Unique 676)) (TyFun () (TyBuiltin ()
+(SomeTypeIn DefaultUniInteger)) (TyFun () (TyBuiltin () (SomeTypeIn
+DefaultUniInteger)) (TyBuiltin () (SomeTypeIn DefaultUniInteger))))) (Builtin ()
+AddInteger) :| []) (Let () NonRec (TermBind () NonStrict (VarDecl () (Name
+[x61,x64,x64,x49,x6e,x74,x65,x67,x65,x72] (Unique 682)) (TyFun () (TyBuiltin ()
+(SomeTypeIn DefaultUniInteger)) (TyFun () (TyBuiltin () (SomeTypeIn
+DefaultUniInteger)) (TyBuiltin () (SomeTypeIn DefaultUniInteger))))) (LamAbs ()
+(Name [x78] (Unique 678)) (TyBuiltin () (SomeTypeIn DefaultUniInteger)) (LamAbs
+() (Name [x79] (Unique 679)) (TyBuiltin () (SomeTypeIn DefaultUniInteger)) (Let
+() NonRec (TermBind () Strict (VarDecl () (Name [x78] (Unique 680)) (TyBuiltin
+() (SomeTypeIn DefaultUniInteger))) (Var () (Name [x78] (Unique 678))) :| [])
+(Let () NonRec (TermBind () Strict (VarDecl () (Name [x79] (Unique 681))
+(TyBuiltin () (SomeTypeIn DefaultUniInteger))) (Var () (Name [x79] (Unique
+679))) :| []) (Apply () (Apply () (Var () (Name
+[x61,x64,x64,x49,x6e,x74,x65,x67,x65,x72] (Unique 676))) (Var () (Name [x78]
+(Unique 680)))) (Var () (Name [x79] (Unique 681)))))))) :| []) (Let () Rec
+(DatatypeBind () (Datatype () (TyVarDecl () (TyName (Name [x4d,x61,x79,x62,x65]
+(Unique 683))) (KindArrow () (Type ()) (Type ()))) [TyVarDecl () (TyName (Name
+[x61] (Unique 687))) (Type ())] (Name
+[x4d,x61,x79,x62,x65,x5f,x6d,x61,x74,x63,x68] (Unique 686)) [VarDecl () (Name
+[x4a,x75,x73,x74] (Unique 684)) (TyFun () (TyVar () (TyName (Name [x61] (Unique
+687)))) (TyApp () (TyVar () (TyName (Name [x4d,x61,x79,x62,x65] (Unique 683))))
+(TyVar () (TyName (Name [x61] (Unique 687)))))),VarDecl () (Name
+[x4e,x6f,x74,x68,x69,x6e,x67] (Unique 685)) (TyApp () (TyVar () (TyName (Name
+[x4d,x61,x79,x62,x65] (Unique 683)))) (TyVar () (TyName (Name [x61] (Unique
+687)))))]) :| []) (Let () NonRec (TermBind () NonStrict (VarDecl () (Name [x66]
+(Unique 719)) (TyFun () (TyApp () (TyVar () (TyName (Name [x4d,x61,x79,x62,x65]
+(Unique 683)))) (TyBuiltin () (SomeTypeIn DefaultUniInteger))) (TyFun () (TyApp
+() (TyVar () (TyName (Name [x4d,x61,x79,x62,x65] (Unique 683)))) (TyBuiltin ()
+(SomeTypeIn DefaultUniInteger))) (TyApp () (TyVar () (TyName (Name
+[x4d,x61,x79,x62,x65] (Unique 683)))) (TyBuiltin () (SomeTypeIn
+DefaultUniInteger)))))) (LamAbs () (Name [x64,x73] (Unique 699)) (TyApp ()
+(TyVar () (TyName (Name [x4d,x61,x79,x62,x65] (Unique 683)))) (TyBuiltin ()
+(SomeTypeIn DefaultUniInteger))) (LamAbs () (Name [x64,x73] (Unique 700)) (TyApp
+() (TyVar () (TyName (Name [x4d,x61,x79,x62,x65] (Unique 683)))) (TyBuiltin ()
+(SomeTypeIn DefaultUniInteger))) (Let () NonRec (TermBind () Strict (VarDecl ()
+(Name [x64,x73] (Unique 701)) (TyApp () (TyVar () (TyName (Name
+[x4d,x61,x79,x62,x65] (Unique 683)))) (TyBuiltin () (SomeTypeIn
+DefaultUniInteger)))) (Var () (Name [x64,x73] (Unique 699))) :| []) (Let ()
+NonRec (TermBind () Strict (VarDecl () (Name [x64,x73] (Unique 702)) (TyApp ()
+(TyVar () (TyName (Name [x4d,x61,x79,x62,x65] (Unique 683)))) (TyBuiltin ()
+(SomeTypeIn DefaultUniInteger)))) (Var () (Name [x64,x73] (Unique 700))) :| [])
+(TyInst () (Apply () (Apply () (TyInst () (Apply () (TyInst () (Var () (Name
+[x4d,x61,x79,x62,x65,x5f,x6d,x61,x74,x63,x68] (Unique 686))) (TyBuiltin ()
+(SomeTypeIn DefaultUniInteger))) (Var () (Name [x64,x73] (Unique 701))))
+(TyForall () (TyName (Name [x64,x65,x61,x64] (Unique 706))) (Type ()) (TyApp ()
+(TyVar () (TyName (Name [x4d,x61,x79,x62,x65] (Unique 683)))) (TyBuiltin ()
+(SomeTypeIn DefaultUniInteger))))) (LamAbs () (Name [x78,x27] (Unique 707))
+(TyBuiltin () (SomeTypeIn DefaultUniInteger)) (TyAbs () (TyName (Name
+[x64,x65,x61,x64] (Unique 708))) (Type ()) (TyInst () (Apply () (Apply ()
+(TyInst () (Apply () (TyInst () (Var () (Name
+[x4d,x61,x79,x62,x65,x5f,x6d,x61,x74,x63,x68] (Unique 686))) (TyBuiltin ()
+(SomeTypeIn DefaultUniInteger))) (Var () (Name [x64,x73] (Unique 702))))
+(TyForall () (TyName (Name [x64,x65,x61,x64] (Unique 712))) (Type ()) (TyApp ()
+(TyVar () (TyName (Name [x4d,x61,x79,x62,x65] (Unique 683)))) (TyBuiltin ()
+(SomeTypeIn DefaultUniInteger))))) (LamAbs () (Name [x79,x27] (Unique 713))
+(TyBuiltin () (SomeTypeIn DefaultUniInteger)) (TyAbs () (TyName (Name
+[x64,x65,x61,x64] (Unique 714))) (Type ()) (Apply () (TyInst () (Var () (Name
+[x4a,x75,x73,x74] (Unique 684))) (TyBuiltin () (SomeTypeIn DefaultUniInteger)))
+(Apply () (Apply () (Var () (Name [x61,x64,x64,x49,x6e,x74,x65,x67,x65,x72]
+(Unique 682))) (Var () (Name [x78,x27] (Unique 707)))) (Var () (Name [x79,x27]
+(Unique 713)))))))) (TyAbs () (TyName (Name [x64,x65,x61,x64] (Unique 715)))
+(Type ()) (TyInst () (Var () (Name [x4e,x6f,x74,x68,x69,x6e,x67] (Unique 685)))
+(TyBuiltin () (SomeTypeIn DefaultUniInteger))))) (TyForall () (TyName (Name
+[x64,x65,x61,x64] (Unique 716))) (Type ()) (TyVar () (TyName (Name
+[x64,x65,x61,x64] (Unique 716))))))))) (TyAbs () (TyName (Name [x64,x65,x61,x64]
+(Unique 717))) (Type ()) (TyInst () (Var () (Name [x4e,x6f,x74,x68,x69,x6e,x67]
+(Unique 685))) (TyBuiltin () (SomeTypeIn DefaultUniInteger))))) (TyForall ()
+(TyName (Name [x64,x65,x61,x64] (Unique 718))) (Type ()) (TyVar () (TyName (Name
+[x64,x65,x61,x64] (Unique 718)))))))))) :| []) (Apply () (Apply () (TyInst ()
+(Apply () (TyInst () (Var () (Name [x4d,x61,x79,x62,x65,x5f,x6d,x61,x74,x63,x68]
+(Unique 686))) (TyBuiltin () (SomeTypeIn DefaultUniInteger))) (Apply () (Apply
+() (Var () (Name [x66] (Unique 719))) (Apply () (TyInst () (Var () (Name
+[x4a,x75,x73,x74] (Unique 684))) (TyBuiltin () (SomeTypeIn DefaultUniInteger)))
+(Constant () (Some (ValueOf DefaultUniInteger 3))))) (Apply () (TyInst () (Var
+() (Name [x4a,x75,x73,x74] (Unique 684))) (TyBuiltin () (SomeTypeIn
+DefaultUniInteger))) (Constant () (Some (ValueOf DefaultUniInteger 5))))))
+(TyBuiltin () (SomeTypeIn DefaultUniInteger))) (LamAbs () (Name [x6e] (Unique
+723)) (TyBuiltin () (SomeTypeIn DefaultUniInteger)) (Var () (Name [x6e] (Unique
+723))))) (Constant () (Some (ValueOf DefaultUniInteger 0)))))))
+.
 
+
+Definition maybeFun__optimised :=
+
+Let () NonRec (DatatypeBind () (Datatype () (TyVarDecl () (TyName (Name
+[x4d,x61,x79,x62,x65] (Unique 1186))) (KindArrow () (Type ()) (Type ())))
+[TyVarDecl () (TyName (Name [x61] (Unique 1190))) (Type ())] (Name
+[x4d,x61,x79,x62,x65,x5f,x6d,x61,x74,x63,x68] (Unique 1189)) [VarDecl () (Name
+[x4a,x75,x73,x74] (Unique 1187)) (TyFun () (TyVar () (TyName (Name [x61] (Unique
+1190)))) (TyApp () (TyVar () (TyName (Name [x4d,x61,x79,x62,x65] (Unique
+1186)))) (TyVar () (TyName (Name [x61] (Unique 1190)))))),VarDecl () (Name
+[x4e,x6f,x74,x68,x69,x6e,x67] (Unique 1188)) (TyApp () (TyVar () (TyName (Name
+[x4d,x61,x79,x62,x65] (Unique 1186)))) (TyVar () (TyName (Name [x61] (Unique
+1190)))))]) :| []) (Constant () (Some (ValueOf DefaultUniInteger 8)))
 .
 
